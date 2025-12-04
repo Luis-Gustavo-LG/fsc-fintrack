aedac11dba7100f91859393f5ea8ba47a81c932a
Luis-Gustavo-LG <nickgrs07@gmail.com>
2025-12-04 06:01:48 -0300

    chore: remove husky

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,22 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -24,12 +27,20 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
-    plugins: [eslintPluginSimpleImportSort],
+
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
     },
   },
-  eslintConfigPrettier,
+
+  {
+    name: 'prettier',
+    ...eslintConfigPrettier,
+  },
 ])
